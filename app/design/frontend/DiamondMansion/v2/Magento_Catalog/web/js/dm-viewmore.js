f6061d59afe2222145fd33c44fe8e3ecb41dfa4b
maysoft1116 <maysoft1116@outlook.com>
2019-02-19 03:22:27 +1000

    Implemented Lazy Load

--- app/design/frontend/DiamondMansion/v2/Magento_Catalog/web/js/dm-viewmore.js
+++ app/design/frontend/DiamondMansion/v2/Magento_Catalog/web/js/dm-viewmore.js
@@ -22,6 +22,8 @@ define([
                     onViewMoreProducts();
                 }
             });
+
+            onViewMoreProducts();
         });
     
         $(window).load(function () {
