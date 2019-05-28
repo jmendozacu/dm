93a050b187ee7c5ede5243b1ce5032428d2f4c82
maysoft1116 <maysoft1116@outlook.com>
2019-05-29 00:07:24 +1000

    update attribute migration for fixed site stones

--- app/code/DiamondMansion/Extensions/Console/Command/MigrateAttribute.php
+++ app/code/DiamondMansion/Extensions/Console/Command/MigrateAttribute.php
@@ -76,29 +76,97 @@ class MigrateAttribute extends Command
 
         $m1Connect = new \mysqli($host, $user, $pass, $db) or die ('Connect failed to magento 1 database');
 
-        $query = "SELECT c.* FROM catalog_product_entity_" . $type . " as c LEFT JOIN eav_attribute AS e ON c.attribute_id=e.attribute_id WHERE c.`value` IS NOT NULL AND e.attribute_code='" . $src . "'";
-        $result = $m1Connect->query($query);
-
         $count = 0;
-        if ($result->num_rows > 0) {
-            $rows = $this->_connection->fetchAll("SELECT attribute_id FROM eav_attribute WHERE attribute_code='" . $dst . "'");
-            foreach ($rows as $row) {
-                $dstId = $row['attribute_id'];
-                
-                $query = "DELETE FROM catalog_product_entity_" . $type . " WHERE attribute_id=" . $dstId;
-                $this->_connection->query($query);
-    
-                while ($row = $result->fetch_assoc()) {
-                    try {
-                        $query = "INSERT INTO catalog_product_entity_" . $type . " (`attribute_id`, `store_id`, `entity_id`, `value`) VALUES (" . $dstId . ", 0, " . $row['entity_id'] . ", '" . $row['value'] . "')";
-                        $this->_connection->query($query);
-                        $count ++;
-                    } catch (\Exception $e) {
-                        $output->writeln('<error>Product ' . $row['entity_id'] . ' Error.</error>');
+        if ($type == 'side-stone') {
+            $srcIds = [
+                [247, 242, 252],
+                [248, 243, 253],
+                [249, 244, 254],
+                [250, 245, 255],
+            ];
+            $counter = 0;
+            foreach ($srcIds as $srcId) {
+                $counter ++;
+                $query = "SELECT i.entity_id, v.`value` FROM catalog_product_entity_int AS i LEFT JOIN eav_attribute_option_value AS v ON i.`value`=v.`option_id` WHERE i.attribute_id=" . $srcId[0] . " AND i.`value` IS NOT NULL";
+                $result = $m1Connect->query($query);
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        $query = "SELECT i.`value` FROM catalog_product_entity_varchar AS i WHERE i.attribute_id=" . $srcId[1] . " AND i.entity_id=" . $row['entity_id'] . " AND (i.`value` IS NOT NULL OR i.`value` != 0)";
+                        $caratResult1 = $m1Connect->query($query);
+                        $carat1 = 0;
+                        if ($caratResult1->num_rows > 0) {
+                            while ($caratRow1 = $caratResult1->fetch_assoc()) {
+                                $carat1 = floatval($caratRow1['value']);
+                                break;
+                            }
+                        }
+
+                        $query = "SELECT i.`value` FROM catalog_product_entity_varchar AS i WHERE i.attribute_id=" . $srcId[2] . " AND i.entity_id=" . $row['entity_id'] . " AND (i.`value` IS NOT NULL OR i.`value` != 0)";
+                        $caratResult2 = $m1Connect->query($query);
+                        $carat2 = 0;
+                        if ($caratResult2->num_rows > 0) {
+                            while ($caratRow2 = $caratResult2->fetch_assoc()) {
+                                $carat2 = floatval($caratRow2['value']);
+                                break;
+                            }
+                        }
+
+                        $sku = strtolower(substr($row['value'], 0, 1));
+                        if ($row['value'] == 'Princess') {
+                            $sku = 'q';
+                        } else if ($row['value'] == 'Radiant') {
+                            $sku = 'i';
+                        } else if ($row['value'] == 'Trapezoid') {
+                            $sku = 'g';
+                        } else if ($row['value'] == 'Halfmoon') {
+                            $sku = 'f';
+                        } else if ($row['value'] == 'Bullet') {
+                            $sku = 'd';
+                        }
+
+                        if ($carat1 > 0 || $carat2 > 0) {
+                            $query = "DELETE FROM dm_product_options WHERE `group` LIKE 'side-stone-%' AND product_id=" . $row['entity_id'];
+                            $this->_connection->query($query);
+
+                            try {
+                                $query = "INSERT INTO dm_product_options (`product_id`, `group`, `code`, `title`, `slug`, `is_default`, `values`) VALUES (" . $row['entity_id'] . ", 'side-stone-shape-" . $counter . "', '" . strtolower($row['value']) . "', '" . $row['value'] . "', '" . $sku . "', 1, '" . json_encode(['qty'=>[$carat1/0.01, $carat2/0.01]]) . "')";
+                                $this->_connection->query($query);
+                                $query = "INSERT INTO dm_product_options (`product_id`, `group`, `code`, `title`, `slug`, `is_default`, `values`) VALUES (" . $row['entity_id'] . ", 'side-stone-carat-" . $counter . "', '0.01', '0.01', 'b', 1, '')";
+                                $this->_connection->query($query);
+                                $query = "INSERT INTO dm_product_options (`product_id`, `group`, `code`, `title`, `slug`, `is_default`, `values`) VALUES (" . $row['entity_id'] . ", 'side-stone-color-clarity-" . $counter . "', 'f-g/vs', 'F-G/VS', 'f', 1, '')";
+                                $this->_connection->query($query);
+                                $count ++;
+                            } catch (\Exception $e) {
+                                $output->writeln('<error>Product ' . $row['entity_id'] . ' Error.</error>');
+                            }
+                        }
                     }
                 }
+            }
+        } else {
+            $query = "SELECT c.* FROM catalog_product_entity_" . $type . " as c LEFT JOIN eav_attribute AS e ON c.attribute_id=e.attribute_id WHERE c.`value` IS NOT NULL AND e.attribute_code='" . $src . "'";
+            $result = $m1Connect->query($query);
 
-                break;
+            if ($result->num_rows > 0) {
+                $rows = $this->_connection->fetchAll("SELECT attribute_id FROM eav_attribute WHERE attribute_code='" . $dst . "'");
+                foreach ($rows as $row) {
+                    $dstId = $row['attribute_id'];
+                    
+                    $query = "DELETE FROM catalog_product_entity_" . $type . " WHERE attribute_id=" . $dstId;
+                    $this->_connection->query($query);
+        
+                    while ($row = $result->fetch_assoc()) {
+                        try {
+                            $query = "INSERT INTO catalog_product_entity_" . $type . " (`attribute_id`, `store_id`, `entity_id`, `value`) VALUES (" . $dstId . ", 0, " . $row['entity_id'] . ", '" . $row['value'] . "')";
+                            $this->_connection->query($query);
+                            $count ++;
+                        } catch (\Exception $e) {
+                            $output->writeln('<error>Product ' . $row['entity_id'] . ' Error.</error>');
+                        }
+                    }
+
+                    break;
+                }
             }
         }
 
