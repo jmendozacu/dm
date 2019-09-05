31c77c4b0bf657428d07f0ffe252126b63ce515b
maysoft1116 <maysoft1116@outlook.com>
2019-09-05 20:55:37 +1000

    WIP: minicart updates

---
+++ app/code/DiamondMansion/Extensions/Override/Magento/Checkout/CustomerData/DefaultItem.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace DiamondMansion\Extensions\Override\Magento\Checkout\CustomerData;
+
+class DefaultItem extends \Magento\Checkout\CustomerData\DefaultItem
+{
+    protected function doGetItemData() {
+        $product = $this->item->getProduct();
+
+        $imageHelper = $this->imageHelper->init($this->getProductForThumbnail(), 'mini_cart_product_thumbnail');
+        $productName = $product->getDmName();
+
+        return [
+            'options' => $this->getOptionList(),
+            'qty' => $this->item->getQty() * 1,
+            'item_id' => $this->item->getId(),
+            'configure_url' => $this->getConfigureUrl(),
+            'is_visible_in_site_visibility' => $this->item->getProduct()->isVisibleInSiteVisibility(),
+            'product_id' => $this->item->getProduct()->getId(),
+            'product_name' => $productName,
+            'product_sku' => $this->item->getProduct()->getSku(),
+            'product_url' => $product->getProductUrl(),
+            'product_has_url' => $this->hasProductUrl(),
+            'product_price' => $this->checkoutHelper->formatPrice($this->item->getCalculationPrice()),
+            'product_price_value' => $this->item->getCalculationPrice(),
+            'product_image' => [
+                'src' => $imageHelper->getUrl(),
+                'alt' => $imageHelper->getLabel(),
+                'width' => $imageHelper->getWidth(),
+                'height' => $imageHelper->getHeight(),
+            ],
+            'canApplyMsrp' => $this->msrpHelper->isShowBeforeOrderConfirm($this->item->getProduct())
+                && $this->msrpHelper->isMinimalPriceLessMsrp($this->item->getProduct()),
+        ];
+    }
+}
