77ac71ecb8a4e6fc26acc7da24d46d5fb28632e5
maysoft1116 <maysoft1116@outlook.com>
2019-12-05 21:46:39 +1000

    updates for proposal guide page

--- app/design/frontend/DiamondMansion/v2/requirejs-config.js
+++ app/design/frontend/DiamondMansion/v2/requirejs-config.js
@@ -12,7 +12,7 @@ var config = {
             'dm-wishlist': 'js/dm-wishlist',
             'jquery-fancybox': 'js/jquery.fancybox',
             'supersized': 'js/supersized',
-            'responsiveslides': 'js/responsiveslides.min',
+            'responsiveslides': 'js/responsiveslides.min'
         }
     },
     paths: {
@@ -43,7 +43,24 @@ var config = {
 
         'slick': 'js/slick.min',
         'scrollmagic': 'js/scrollmagic.min',
-        'modernizr': 'js/modernizr.min'
+        'modernizr': 'js/modernizr.min',
+
+        'proposal-guide-preloader': 'proposal-guide/js/preloader/jquery.queryloader2',
+        'proposal-guide-bootstrap': 'proposal-guide/js/bootstrap/bootstrap',
+        'proposal-guide-easing': 'proposal-guide/js/supersized/jquery.easing.min',
+        'proposal-guide-supersized': 'proposal-guide/js/supersized/supersized.3.2.7.min',
+        'proposal-guide-shutter': 'proposal-guide/js/supersized/supersized.shutter.min',
+        'proposal-guide-sticky': 'proposal-guide/js/sticky/jquery.sticky',
+        'proposal-guide-nav': 'proposal-guide/js/navjs/jquery.nav',
+        'proposal-guide-nicescroll': 'proposal-guide/js/nicescroll/jquery.nicescroll.min',
+        'proposal-guide-waypoints': 'proposal-guide/js/waypoint/waypoints.min',
+        'proposal-guide-gmap': 'proposal-guide/js/gmap/jquery.gmap.min',
+        'proposal-guide-popup': 'proposal-guide/js/magnific-popup/jquery.magnific-popup.min',
+        'proposal-guide-masonry': 'proposal-guide/js/masonry/masonry.pkgd.min',
+        'proposal-guide-owlcarousel': 'proposal-guide/js/owlcarousel/owl.carousel',
+        'proposal-guide-rsvp': 'proposal-guide/js/rsvp',
+        'proposal-guide-swithcer': 'proposal-guide/js/style-swithcer/stylesheet-switcher',
+        'proposal-guide-script': 'proposal-guide/js/script'
     },
     shim: {
         'philanthropy-bootstrap': {
@@ -119,6 +136,55 @@ var config = {
         },
         'modernizr': {
             deps: []
+        },
+
+        'proposal-guide-preloader': {
+            deps: ['jquery']
+        },
+        'proposal-guide-bootstrap': {
+            deps: ['jquery']
+        },
+        'proposal-guide-easing': {
+            deps: ['jquery']
+        },
+        'proposal-guide-supersized': {
+            deps: ['jquery']
+        },
+        'proposal-guide-shutter': {
+            deps: ['jquery']
+        },
+        'proposal-guide-sticky': {
+            deps: ['jquery']
+        },
+        'proposal-guide-nav': {
+            deps: ['jquery']
+        },
+        'proposal-guide-nicescroll': {
+            deps: ['jquery']
+        },
+        'proposal-guide-waypoints': {
+            deps: ['jquery']
+        },
+        'proposal-guide-gmap': {
+            deps: ['jquery']
+        },
+        'proposal-guide-popup': {
+            deps: ['jquery']
+        },
+        'proposal-guide-masonry': {
+            deps: ['jquery']
+        },
+        'proposal-guide-owlcarousel': {
+            deps: ['jquery']
+        },
+        'proposal-guide-rsvp': {
+            deps: ['jquery']
+        },
+        'proposal-guide-swithcer': {
+            deps: ['jquery']
+        },
+        'proposal-guide-script': {
+            deps: ['jquery']
         }
     }
 };
